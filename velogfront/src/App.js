a5d9185ab468dfd649593082a070facc32049506
wglee0511 <ranazang@naver.com>
2021-07-16 18:02:26 +0900

    upload components

--- velogfront/src/App.js
+++ velogfront/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import Header from "./component/Header";
+import Detail from "./page/Detail";
+import MainPage from "./page/MainPage";
+import Write from "./page/Write";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={MainPage} />
+        <Route exact path="/detail/:id" component={Detail} />
+        <Route exact path="/white" component={Write} />
+      </Switch>
     </div>
   );
 }
